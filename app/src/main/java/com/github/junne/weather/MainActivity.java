c06e44224782b233479c2fcd2c8371b1301db557
Junne <junnebai@163.com>
2017-11-30 17:10:25 +0800

    Add NavigationView

--- app/src/main/java/com/github/junne/weather/MainActivity.java
+++ app/src/main/java/com/github/junne/weather/MainActivity.java
@@ -1,13 +1,57 @@
 package com.github.junne.weather;
 
+import android.support.annotation.BinderThread;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    @BindView(R.id.nav_view)
+    NavigationView mNavigationView;
+    @BindView(R.id.drawer_layout)
+    DrawerLayout mDrawerLayout;
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initDrawer();
+    }
+
+
+    /**
+     * 初始化抽屉导航
+     */
+
+    private void initDrawer() {
+
+        if (mNavigationView != null) {
+            mNavigationView.setNavigationItemSelectedListener(this);
+            mNavigationView.inflateHeaderView(R.layout.nav_header_main);
+            ActionBarDrawerToggle toggle =
+                    new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.navigation_drawer_open
+                    ,R.string.navigation_drawer_close);
+            mDrawerLayout.addDrawerListener(toggle);
+            toggle.syncState();
+
+        }
+
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
